1ee13891eead51385780929a5375cdce18d1f032
ARNAB KUMAR GHOSH <arnab.kumarghosh.20@aot.edu.in>
2024-03-22 20:08:03 +0530

    Title changed

--- src/components/choose-us/Title.jsx
+++ src/components/choose-us/Title.jsx
@@ -6,7 +6,7 @@ function Title() {
     <div className="mb-10">
       <SecondaryHeading>Why choose us</SecondaryHeading>
       <TertiaryHeading color="white">
-        W can give a shape of your body here!
+        We can give a shape of your body here!
       </TertiaryHeading>
       <p className="font-medium text-gray-200">
         At <b>BoldBody</b>, Our goal is to assist you in getting the body of your desires. Together, you and our knowledgeable trainers and nutritionists will develop a customized exercise and dietary plan that will enable you to accomplish your unique objectives.
